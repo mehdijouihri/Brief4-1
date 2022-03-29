68d11eca7d73608363584b3d0e1f7ab9204b3141
StevenDsimplon <asteven59220@free.fr>
2022-03-29 16:05:11 +0200

    Menu Hamburger

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 /* Sélection des éléments HTML */
-let link = document.getElementById('link')
-let burger = document.getElementById('burger')
-let ul = document.querySelector('ul')
-
+let link = document.getElementById('link');
+let burger = document.getElementById('burger');
+let ul = document.querySelector('ul');
+let nav = document.querySelector('nav');
+let logo = document.getElementById('logo');
 /* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul et au span#burger */
 link.addEventListener('click', function(e) {
   e.preventDefault()
-  burger.classList.toggle('open')
-  ul.classList.toggle('open')
+  nav.classList.toggle('open');
+  logo.classList.toggle('open');
+  burger.classList.toggle('open');
+  ul.classList.toggle('open');
 })
